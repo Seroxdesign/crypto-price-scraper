e360e3aa0e6a1fe0d9715b289440fed151fdff3d
sky cherfa <sherifcherfa@gmail.com>
2022-04-24 12:39:44 -0400

    created and initialised web scraper

---
+++ functions/index.js
@@ -0,0 +1,36 @@
+const functions = require("firebase-functions");
+const cors = require('cors')({origin: true})
+const puppeteer = require('puppeteer');
+
+const scrapeImages = async () => {
+  const browser = await puppeteer.launch( {headless: false} );
+  const page = await browser.newPage();
+
+  await page.goto('https://coinmarketcap.com/currencies/bitcoin/historical-data/', { waitUntil: 'load' });
+
+  await page.waitForSelector('td', {
+    visible: true,
+});
+
+  const data = await page.evaluate( () => {
+
+    const prices = document.querySelectorAll('td');
+    const urls = Array.from(prices).map(v => v.innerText);
+    return urls;
+  })
+
+  await browser.close();
+
+  console.log(data)
+
+  return data
+}
+
+exports.scraper = functions.https.onRequest((request, response) => {
+  cors(request, response, async() => {
+
+    const data = await scrapeImages("seroxdesigns");
+
+    response.send(data)
+  })
+})
